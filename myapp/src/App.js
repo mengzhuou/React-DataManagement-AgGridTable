b073afd55d41b51a99291861d6fe71ec9fbdd6d6
mengzhuou <mou30@gatech.edu>
2024-07-28 17:54:44 -0400

    add click

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,28 +6,52 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 
+import {useState, useRef, useCallback} from 'react';
 
 function App() {
 
-  const rowData =[
+  const gridRef = useRef();
+
+  const [rowData] = useState([
     {name: 'Nina', age: 22, degree: 'bachelor'},
     {name: 'Jack', age: 28, degree: 'bachelor'},
     {name: 'Jan', age: 42, degree: 'high school'}
-  ];
+  ]);
 
-  const columnDefs = [
+  const [columnDefs] = useState([
     {field: 'name'},
     {field: 'age'},
     {field: 'degree'}
-  ];
+  ]);
 
+  const defaultColDef = ()=> (
+    {
+      sortable: true, 
+      filter: true
+    }
+  );
+
+  const cellClickedListener = useCallback( event => {
+    console.log('cellClicked', event);
+  }, []);
+
+  const buttonListener = useCallback( e => {
+    gridRef.current.api.deselectAll();
+  }, []);
 
   return (
-    <div className="ag-theme-alpine" style={{width: 500, height: 500}}>
-      <AgGridReact 
-        rowData={rowData} 
-        columnDefs={columnDefs}
-      />
+    <div>
+      <button onClick={buttonListener}>Push Me</button>
+      <div className="ag-theme-alpine" style={{width: 500, height: 500}}>
+        <AgGridReact 
+          ref={gridRef}
+          rowData={rowData} 
+          columnDefs={columnDefs}
+          animateRows={true} rowSelection='multiple'
+          onCellClicked={cellClickedListener}
+          defaultColDef={defaultColDef}
+        />
+      </div>
     </div>
   );
 }
