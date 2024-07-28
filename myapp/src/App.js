38bd1f4807ce8e683069b8ae4c80aad74ca118d7
mengzhuou <mou30@gatech.edu>
2024-07-28 17:50:12 -0400

    data displayed on aggridtable

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
 
+import {AgGridReact} from 'ag-grid-react';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+
+
 function App() {
+
+  const rowData =[
+    {name: 'Nina', age: 22, degree: 'bachelor'},
+    {name: 'Jack', age: 28, degree: 'bachelor'},
+    {name: 'Jan', age: 42, degree: 'high school'}
+  ];
+
+  const columnDefs = [
+    {field: 'name'},
+    {field: 'age'},
+    {field: 'degree'}
+  ];
+
+
   return (
-    <div>
+    <div className="ag-theme-alpine" style={{width: 500, height: 500}}>
+      <AgGridReact 
+        rowData={rowData} 
+        columnDefs={columnDefs}
+      />
     </div>
   );
 }
